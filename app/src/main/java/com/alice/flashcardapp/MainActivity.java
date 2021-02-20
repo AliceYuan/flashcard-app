ca486bc5c19a11cbd443ffc8d19ae446a12ccbd4
AliceYuan <me@aliceyuan.ca>
2021-02-20 14:55:59 -0500

    Added a click listener and answer question textviews

--- app/src/main/java/com/alice/flashcardapp/MainActivity.java
+++ app/src/main/java/com/alice/flashcardapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.alice.flashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView questionTextView = findViewById(R.id.question);
+        TextView answerTextView = findViewById(R.id.answer);
+
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
